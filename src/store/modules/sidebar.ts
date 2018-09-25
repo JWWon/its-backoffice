fc9b29df39e774f30090973fae24224126c05d76
JWWon <wonjiwn@naver.com>
2018-09-25 21:07:31 +0900

    Create sidebar action on redux

---
+++ src/store/modules/sidebar.ts
@@ -0,0 +1,27 @@
+import { createAction, handleActions } from 'redux-actions';
+
+// *** ACTION TYPE
+const SHOW_SIDEBAR = 'sidebar/SHOW_SIDEBAR';
+const HIDE_SIDEBAR = 'sidebar/HIDE_SIDEBAR';
+
+// *** ACTION FUNCTION
+export const show = createAction(SHOW_SIDEBAR);
+export const hide = createAction(HIDE_SIDEBAR);
+
+// *** INITIAL STATE
+export interface ISidebarState {
+  show: boolean;
+}
+
+const initState: ISidebarState = {
+  show: false,
+};
+
+// *** REDUCER
+export default handleActions<ISidebarState, any>(
+  {
+    [SHOW_SIDEBAR]: state => ({ ...state, show: true }),
+    [HIDE_SIDEBAR]: state => ({ ...state, show: false }),
+  },
+  initState
+);
