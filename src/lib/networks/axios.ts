0dd42a07457fa2a8886139889f34a73ff4fae4a0
JWWon <wonjiwn@naver.com>
2018-11-18 19:36:00 +0900

    remove frontslash from api defaultURL

--- src/lib/networks/axios.ts
+++ src/lib/networks/axios.ts
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 export default function setAxios() {
   if (process.env.NODE_ENV === 'production') {
-    axios.defaults.baseURL = 'https://api.itso-o.com/';
+    axios.defaults.baseURL = 'https://api.itso-o.com';
   } else {
-    axios.defaults.baseURL = 'http://localhost:5000/';
+    axios.defaults.baseURL = 'http://localhost:5000';
   }
   axios.defaults.headers.common['Content-Type'] =
     'application/x-www-form-urlencoded';
